814a7fd9f268eeba88cd62e3117917d8a08e6b6f
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-09 17:02:46 -0300

    feat: add all DTOs with id for model

---
+++ CarsApi.Application/DTOs/Create/CreateModelDto.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsApi.Application.DTOs.Create
+{   public record ModelCreateDto(int Brand_id,string Name,float Fipe_value); }
