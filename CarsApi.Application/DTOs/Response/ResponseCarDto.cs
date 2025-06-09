9d5e9987fe3b700acbb56f4171669499137b9b2d
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-09 17:17:47 -0300

    feat: create all DTOs for car entity

---
+++ CarsApi.Application/DTOs/Response/ResponseCarDto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsApi.Application.DTOs.Create
+{
+    public record ResponseCarDto(int Id,long Timestamp_Cadaster, int Model_id, int Year, string Gas_type,int Num_doors, string Color);
+
+}
