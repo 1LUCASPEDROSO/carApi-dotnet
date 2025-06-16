bcbb5aadfdf48175dc3cfb6fcc196b7bf6a6ba89
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-16 16:19:35 -0300

    refactor: add carService and reposiotry in builder scope

--- CarsApi.API/Program.cs
+++ CarsApi.API/Program.cs
@@ -1,5 +1,7 @@
 
 using CarsApi.Application.Interfaces;
+using CarsApi.Application.Services;
+using CarsApi.Application.Services.Impl;
 using CarsApi.Domain.Services;
 using CarsApi.Domain.Services.Impl;
 using CarsApi.Infrastructure;
@@ -7,19 +9,19 @@ using CarsApi.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+                                        var builder = WebApplication.CreateBuilder(args);
 
+                                        builder.Services.AddDbContext<AppDbContext>(options =>
+                                        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+                                        builder.Services.AddScoped<IBrandService, BrandService>();
+                                        builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+                                        builder.Services.AddScoped<IModelService, ModelService>();
+                                        builder.Services.AddScoped<IModelRepository, ModelRepository>();
+                                        builder.Services.AddScoped<ICarRepository, CarRepository>();
+                                        builder.Services.AddScoped<ICarService, CarService>();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IBrandService, BrandService>();
-builder.Services.AddScoped<IBrandRepository, BrandRepository>();
-builder.Services.AddScoped<IModelService, ModelService>();
-builder.Services.AddScoped<IModelRepository, ModelRepository>();
-
-builder.Services.AddControllers();
+                                        builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
