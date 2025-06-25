fa421cd1ae7b7bfa452fac511b11a4a427f4f061
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-25 18:48:20 -0300

    refactor: create new exeptions implementation

--- CarsApi.Application/Services/Impl/ModelService.cs
+++ CarsApi.Application/Services/Impl/ModelService.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CarsApi.Application.DTOs;
 using CarsApi.Domain.Entities;
 using CarsApi.Application.Interfaces;
 using CarsApi.Application.DTOs.Response;
@@ -24,78 +19,88 @@ namespace CarsApi.Domain.Services.Impl
 
         public async Task<ModelResponseDto> AddModel(ModelCreateDto dto)
         {
-            if (string.IsNullOrWhiteSpace(dto.Name)|| dto.Fipe_value <= 0.00 || dto.Brand_id <= 0)
-            {
-                throw new ArgumentNullException(nameof(dto.Name));
-            }
+            if (string.IsNullOrWhiteSpace(dto.Name))
+                throw new ArgumentNullException(nameof(dto.Name), "Nome é obrigatório");
+
+            if (dto.Fipe_value <= 0)
+                throw new ArgumentException("Fipe_value deve ser maior que 0");
+
+            if (dto.Brand_id <= 0)
+                throw new ArgumentException("Brand_id inválido");
+
             var brand = await _brandRepository.GetBrandById(dto.Brand_id);
             if (brand == null)
-            { 
-                throw new ArgumentNullException("Marca inexistente");
+            {
+                throw new KeyNotFoundException("Marca inexistente");
             }
             var model = new Model { Name = dto.Name, Fipe_value = dto.Fipe_value, Brand_id = dto.Brand_id };
             var createdModel = await _Modelrepository.AddModel(model);
-            return new ModelResponseDto(createdModel.Id,createdModel.Brand_id,createdModel.Name,createdModel.Fipe_value);
+            return new ModelResponseDto(createdModel.Id, createdModel.Brand_id, createdModel.Name, createdModel.Fipe_value);
         }
 
-        public  Task DeleteModel(int Id)
+        public async Task DeleteModel(int Id)
         {
             if (Id <= 0)
             {
-                throw new ArgumentNullException("Id menor que 0 não aceito");
+                throw new ArgumentException("Id inexistente");
             }
-            var model =  _brandRepository.GetBrandById(Id);
+            var model = await _Modelrepository.GetModelById(Id);
             if (model == null)
             {
-                throw new ArgumentNullException("modelo inexistente");
+                throw new KeyNotFoundException("modelo inexistente");
             }
-            return _Modelrepository.DeleteModel(Id);
+            await _Modelrepository.DeleteModel(Id);
         }
 
         public async Task<List<ModelResponseDto>> GetAllModels()
         {
             var models = await _Modelrepository.GetAllModelsAsync();
-            return models.Select(m => new ModelResponseDto(m.Id,m.Brand_id,m.Name,m.Fipe_value)).ToList();
+            return models.Select(m => new ModelResponseDto(m.Id, m.Brand_id, m.Name, m.Fipe_value)).ToList();
         }
 
         public async Task<ModelResponseDto?> GetModelById(int Id)
         {
-            if (Id >= 0)
+            if (Id <= 0)
             {
                 throw new ArgumentException("Id inexistente");
-             }
-            var model = await _Modelrepository.GetModelById(Id);
-            if (model != null) {
-                return new ModelResponseDto(model.Id,model.Brand_id, model.Name, model.Fipe_value);
             }
-            return null;
+            var model = await _Modelrepository.GetModelById(Id);
+            if (model == null)
+            {
+                throw new KeyNotFoundException("modelo inexistente");
+            }
+            return new ModelResponseDto(model.Id, model.Brand_id, model.Name, model.Fipe_value); 
         }
 
         public async Task<ModelResponseDto?> GetModelByName(string name)
         {
             if (string.IsNullOrEmpty(name))
             {
-                throw new ArgumentNullException("Nome invalido");
+                throw new ArgumentNullException("Nome nulo ou invalido");
             }
             var model = await _Modelrepository.GetModelByName(name);
-            return new ModelResponseDto(model.Id,model.Brand_id,model.Name, model.Fipe_value);
-            
+            if (model == null)
+            {
+                throw new KeyNotFoundException("modelo inexistente");
+            }
+            return new ModelResponseDto(model.Id, model.Brand_id, model.Name, model.Fipe_value);
+
         }
 
         public async Task<ModelUpdateDto> UpdateModel(ModelUpdateDto updateDto)
         {
             if (updateDto.Id <= 0 || string.IsNullOrWhiteSpace(updateDto.Name))
             {
-                throw new Exception("Ids menores que zero ou nome null nao aceito ");
+                throw new ArgumentNullException("Ids menores que zero ou nome null nao aceito ");
             }
             var brand = await _brandRepository.GetBrandById(updateDto.Brand_id);
             if (brand == null)
-            { 
-                throw new ArgumentNullException("Marca inexistente");
+            {
+                throw new KeyNotFoundException("marca inexistente");
             }
-            var updatedModel = new Model { Brand_id = updateDto.Brand_id,Name = updateDto.Name, Fipe_value = updateDto.Fipe_value };
+            var updatedModel = new Model { Brand_id = updateDto.Brand_id, Name = updateDto.Name, Fipe_value = updateDto.Fipe_value };
             var model = await _Modelrepository.UpdateModel(updateDto.Id, updatedModel);
-            return new ModelUpdateDto(model.Id,model.Fipe_value,model.Name,model.Brand_id);
+            return new ModelUpdateDto(model.Id, model.Fipe_value, model.Name, model.Brand_id);
         }
     }
 }
