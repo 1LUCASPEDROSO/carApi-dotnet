f6e25f82ff2657436ff4e8b1663bfbc6ec77f8d4
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-25 18:48:51 -0300

    refactor: use produces and responsetypes for docs swagger

--- CarsApi.API/Controllers/ModelController.cs
+++ CarsApi.API/Controllers/ModelController.cs
@@ -11,6 +11,7 @@ namespace CarsApi.API.Controllers
 {
     [ApiController]
     [Route("api/models")]
+    [Produces("application/json")]
     public class ModelController : ControllerBase
     {
         private readonly IModelService _modelService;
@@ -19,20 +20,26 @@ namespace CarsApi.API.Controllers
         {
             _modelService = modelService;
         }
-
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAllModels()
         {
             var models = await _modelService.GetAllModels();
             return Ok(models);
         }
         [HttpGet("id/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetModelById(int id)
         {
             var model = await _modelService.GetModelById(id);
             return Ok(model);
         }
         [HttpGet("name/{name}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetModelByName(String name)
         {
             var model = await _modelService.GetModelByName(name);
@@ -40,22 +47,29 @@ namespace CarsApi.API.Controllers
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> AddModel(ModelCreateDto dto)
         {
             var model = await _modelService.AddModel(dto);
             return Ok(model);
         }
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateModel(ModelUpdateDto dto)
         {
             var updatedModel = await _modelService.UpdateModel(dto);
             return Ok(updatedModel);
         }
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteModel(int Id)
         {
             await _modelService.DeleteModel(Id);
             return Ok();
-        } 
+        }
     }
 }
