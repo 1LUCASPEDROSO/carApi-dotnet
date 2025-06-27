a2cb893794fe3fe42d5743c893e34b2d34bbdc0e
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-27 14:53:35 -0300

    feat: add nre migrastion, modify car entity to use fuel_type

---
+++ CarsApi.Infrastructure/Migrations/20250627173345_modifyCarEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarsApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifyCarEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            "ALTER TABLE \"Cars\" ALTER COLUMN \"Gas_type\" TYPE integer USING \"Gas_type\"::integer;"
+        );
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Cars\" ALTER COLUMN \"Gas_type\" TYPE text USING \"Gas_type\"::text;"
+            );
+        }
+
+    }
+}
