636a674be0438d44308d3b1539f3115a7231cdb1
lucas pedroso <pedrosolucas1745@gmail.com>
2025-07-25 15:43:25 -0300

    feat: create All dtos for role

---
+++ CarsApi.Application/DTOs/Update/UpdateRoleDto.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsApi.Application.DTOs.Update
+{
+    public record UpdateRoleDto(int Id,string Name);
+}
