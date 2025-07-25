333719ec0bf5aebf66739d29a12c67c4d13ca5be
lucas pedroso <pedrosolucas1745@gmail.com>
2025-07-25 15:55:53 -0300

    refactor: modify brand controller to use autorization in post,delete and update and produces only json, fix delete route do pass lowercase id

--- CarsApi.API/Controllers/BrandController.cs
+++ CarsApi.API/Controllers/BrandController.cs
@@ -6,11 +6,12 @@ using CarsApi.Application.DTOs.Create;
 using CarsApi.Application.DTOs.Response;
 using CarsApi.Application.DTOs.Update;
 using CarsApi.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CarsApi.API.Controllers
-{
+namespace CarsApi.API.Controllers{
     [ApiController]
+    [Produces("application/json")]
     [Route("api/brands")]
     public class BrandController : ControllerBase
     {
@@ -22,40 +23,54 @@ namespace CarsApi.API.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<IActionResult> GetAllBrands()
         {
             var brands = await _brandService.GetAllBrands();
             return Ok(brands);
         }
         [HttpGet("id/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<IActionResult> GetBrandById(int id)
         {
             var brands = await _brandService.GetBrandById(id);
             return Ok(brands);
         }
         [HttpGet("name/{name}")]
-        public async Task<IActionResult> GetBrandByName(String name)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<IActionResult> GetBrandByName(string name)
         {
             var brands = await _brandService.GetBrandByName(name);
             return Ok(brands);
         }
-
+        [Authorize(Roles = "admin,brand-manager")]
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> AddBrand(BrandCreateDto dto)
         {
             var brands = await _brandService.AddBrand(dto);
             return Ok(brands);
         }
+        [Authorize(Roles = "admin,brand-manager")]
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> UpdateBrand(BrandUpdateDto dto)
         {
             var upddatedBrand = await _brandService.UpdateBrand(dto);
             return Ok(upddatedBrand);
         }
+        [Authorize(Roles = "admin,brand-manager")]
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBrand(int Id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteBrand(int id)
         {
-            await _brandService.DeleteBrand(Id);
+            await _brandService.DeleteBrand(id);
             return Ok();
         }
     }
