c1cd178c71f79bbc3d60c2c370e272ccc387820c
lucas pedroso <pedrosolucas1745@gmail.com>
2025-07-25 16:10:40 -0300

    refactor:  register new services, configure to use token JWT and create ne version of api

--- CarsApi.API/Program.cs
+++ CarsApi.API/Program.cs
@@ -1,12 +1,17 @@
 
+using System.Text;
+using System.Text.RegularExpressions;
 using CarsApi.Application.Interfaces;
+using CarsApi.Application.security;
 using CarsApi.Application.Services;
 using CarsApi.Application.Services.Impl;
 using CarsApi.Domain.Services;
 using CarsApi.Domain.Services.Impl;
 using CarsApi.Infrastructure;
 using CarsApi.Infrastructure.Persistence;
+using CarsApi.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,13 +19,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddTransient<TokenService>();
 builder.Services.AddScoped<IBrandService, BrandService>();
 builder.Services.AddScoped<IBrandRepository, BrandRepository>();
 builder.Services.AddScoped<IModelService, ModelService>();
 builder.Services.AddScoped<IModelRepository, ModelRepository>();
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 builder.Services.AddScoped<ICarService, CarService>();
-
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRolesRepository, UserRoleRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhostFrontend", policy =>
@@ -30,11 +41,55 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod();
     });
 });
+var key1 = builder.Configuration["JwtSettings:SecretKey"];
+var key = Encoding.UTF8.GetBytes(key1);
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidateIssuer = true,
+    ValidIssuer = "CarsApi",
+    ValidateAudience = true,
+    ValidAudience = "CarsApiClient",
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(key),
+    RoleClaimType = "roles" // opcional, mas importante para múltiplas roles
+};
+
+    });
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cars API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cars API - carsOps", Version = "v2" });
+     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Digite o token no formato: Bearer {seu token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 var app = builder.Build();
@@ -45,6 +100,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseMiddleware<GlobalMiddleware>();
+app.UseAuthentication(); 
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
