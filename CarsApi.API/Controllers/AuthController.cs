761f2089c16784af34682c815cc25ec64092e679
lucas pedroso <pedrosolucas1745@gmail.com>
2025-07-25 16:08:15 -0300

    feat: create authController nor using authorization

---
+++ CarsApi.API/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+using CarsApi.Application.DTOs.SecurityDto;
+using CarsApi.Application.security;
+using CarsApi.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsApi.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+        private readonly  TokenService tokenService;
+        public AuthController(IAuthService authService1, TokenService tokenService1)
+        {
+            authService = authService1;
+            tokenService = tokenService1;
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Login(RequestAuthDto dto)
+        {
+            var user = await authService.GetUserByLogin(dto);
+            if (user == null)
+            {
+                throw new Exception("usuario nao encontrado");
+            }
+            var filtredUser = new ResponseAuthDto(
+                user.Id,
+                user.Name,
+                user.Email,
+                user.Roles
+            );
+            var token = tokenService.Generate(filtredUser);
+            return Ok(token);
+        }
+    }
+}
